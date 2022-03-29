4a122a02d83eb42409ac9e06bc29a43750d647ba
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-03-29 16:32:50 +0530

    fuction call wrap fun,  onclick event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick  = (e) =>{
+        console.log('hello')
+        console.log(e)
+    }
+
+    const handleClickArg = (name, e) =>{
+        console.log('hello ' + name)
+        console.log(e.target)
+    }
     return (
         <div className="home">
         <h2>Home Page</h2>
+        <button onClick={ handleClick }>Handle click</button>
+        <button onClick={ (e) => handleClickArg('shyam', e) }>Handle click Argument</button>
         </div>
       );
 }
