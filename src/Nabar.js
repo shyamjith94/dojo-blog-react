0cf9f8166f28f22868eacc3d7097d959a808e933
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-03-29 13:14:01 +0530

    multiple component and navbar

---
+++ src/Nabar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <nav className="Navbar">
+            <h1>The Dojo Blogs</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
