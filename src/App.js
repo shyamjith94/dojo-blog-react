e176a87bc4dcd1d5f7bea997f97f2d4fb08fef18
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-03-29 12:34:12 +0530

    variable and dyamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 function App() {
+  const title= "welcome to new blog";
+  const likes = 50;
+  const googleLink = "http://www.google.com";
   return (
     <div className="App">
      <div className="content">
-       <h1>app component</h1>
+       <h1>{ title }</h1>
+       <p>likes {likes} times</p>
+       <p>{ [1, 2 , 3, 4, 5] }</p>
+       <p>{ 50 }</p>
+       <p>{ Math.random()*10 }</p>
+       <a href={ googleLink }> google site </a>
      </div>
     </div>
   );
