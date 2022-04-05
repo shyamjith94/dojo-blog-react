d8422d9fce97d01694149eb261c494db07b6ca18
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-04-05 12:37:56 +0530

    fetch data from api using usetEffect and set to useState

--- src/Home.js
+++ src/Home.js
@@ -2,32 +2,25 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "my new website 1", body: "welcome 1", author: "ramas", id:1},
-        {title: "my new website 2", body: "welcome 2", author: "kiran", id:2},
-        {title: "my new website 3", body: "welcome 3", author: "krish", id:3},
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState('ramas')
 
-    //delete blog
-    const handleDelete = (id) =>{
-        const newBlog = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlog);
-    }
 
     useEffect( () => {
-        console.log("use effecrt")
-        console.log(name)
-    }, [name]);
+        // fetch data ans set to blog array
+        fetch('http://localhost:8000/blog').then(
+            res => {
+                return res.json()
+            }
+        ).then(data => { setBlogs(data) })
+    }, []);
 
 
     return (
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs!" handleDelete = { handleDelete }/>
-            <BlogList blogs={ blogs.filter((blog)=> blog.author === 'kiran') } title="kiran blogs!" handleDelete={ handleDelete }/>
-            <button onClick={ () => setName('kiran') }> change name </button>
-            <p> {name} </p>
+             {/* if blogs && evaluvate true then only <BlogList blogs={ blogs } title="All Blogs!" handleDelete = { handleDelete }/> execute */}
+            {blogs && <BlogList blogs={ blogs } title="All Blogs!"/>}
+            
         </div>
       );
 }
