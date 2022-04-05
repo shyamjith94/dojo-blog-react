351297789f740c684684dfbb9cdfb57a74d5aa43
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-04-05 17:45:46 +0530

    react router and switch component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Navbar from './Nabar';
 import Home from './Home';
+import Create from './Create';
+
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
+    // react router
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
+
   );
 }
 
